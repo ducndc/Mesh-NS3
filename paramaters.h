83393afd84f574211fd0769412c083a70680d1c0
ducndc <nguyendangchungduc1999@gmail.com>
2023-04-02 15:28:59 +0700

    Update mesh.cc paramater.h CMakeList.txt

---
+++ paramaters.h
@@ -0,0 +1,12 @@
+#define X_SIZE 5
+#define Y_SIZE 5
+
+#define STEP_NUM 50
+#define RANDOM_START 0.1
+
+#define TOTAL_TIME 100.0
+#define PACKET_INTERVAL 1
+#define PACKET_SIZE 1024 
+
+#define IFACE_NUM 1
+
