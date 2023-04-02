4a1fdf061469ecf8597a0da12f1a30939ee34cd1
ducndc <nguyendangchungduc1999@gmail.com>
2023-04-02 15:46:48 +0700

    Update comments of pramaters in mesh network

--- paramaters.h
+++ paramaters.h
@@ -1,12 +1,12 @@
-#define X_SIZE 5
-#define Y_SIZE 5
+#define X_SIZE 3 ///Number of nodes in a row grid
+#define Y_SIZE 3 ///Number of rows in a grid
 
-#define STEP_NUM 50
-#define RANDOM_START 0.1
+#define STEP_NUM 50 ///Size of edge in our grid (meters)
+#define RANDOM_START 0.1 ///Maximum random start delay for beacon jitter (sec)
 
-#define TOTAL_TIME 100.0
-#define PACKET_INTERVAL 1
-#define PACKET_SIZE 1024 
+#define TOTAL_TIME 1000.0 ///Simulation time (sec)
+#define PACKET_INTERVAL 1 ///Interval between packets in UDP ping (sec)
+#define PACKET_SIZE 1024 ///Size of packets in UDP ping (bytes)
 
-#define IFACE_NUM 1
+#define IFACE_NUM 1 ///Number of radio interfaces used by each mesh point
 
