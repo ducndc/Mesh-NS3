43ddcd62afef7b42c4f6af849d882dc0fd4f89a6
ducndc <nguyendangchungduc1999@gmail.com>
2023-04-02 19:21:47 +0700

    Update author

--- paramaters.h
+++ paramaters.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2008,2009 IITP RAS
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Chung Duc Nguyen Dang <nguyendangchungduc1999@gmail.com>
+ *
+ */
+
 #define X_SIZE 3 ///Number of nodes in a row grid
 #define Y_SIZE 3 ///Number of rows in a grid
 
